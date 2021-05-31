f02cca410285dd24762ef19e65f98a47cc43b44b
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-05-31 11:35:34 +0700

    ADD duration and endtime

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,42 +1,44 @@
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
+
 
 contract Lottery {
     address public manager;
     address public prevWinner;
     uint public prevPrize;
     address[] public players;
+    uint public endTime;
+    uint duration;
     
-    constructor() {
+    constructor(uint hours_duration) {
         manager = msg.sender;
+        duration = hours_duration * 1 hours;
+        endTime = block.timestamp + duration;
     }
     
     function enter() public payable {
         require(msg.value >= 0.01 ether);
-        // The money that send is in the balance of this contract
         players.push(msg.sender);
     }
     
-    function getPlayers() external view returns (address[] memory) {
+    function getNumberOfPlayers() external view returns (address[] memory) {
         return players;
     }
     
     function random() private view returns (uint) {
-        // pseudorandom of blockdifficulty, timestamp, and players
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players, address(this).balance)));
     }
     
-    modifier restricted() {
-        require(msg.sender == manager);
-        _;
-    }
     
-    function pickWinner() public restricted {
+    function pickWinner() public {
+        require(block.timestamp >= endTime);
         uint index = random() % players.length;
-        // Send all of ether in the balance of contract to the winner
         address payable winner = payable(players[index]);
         prevPrize = address(this).balance;
-        winner.transfer(address(this).balance);
         prevWinner = players[index];
+        winner.transfer(address(this).balance);
         players = new address[](0);
+        endTime = block.timestamp + duration;
     }
+    
+
 }
